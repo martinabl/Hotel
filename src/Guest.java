15b6651c226814f80ca0ff66c71809785571bba2
martinabl <matinka271664@gmail.com>
2024-10-06 16:36:00 +0200

    Změny
    vytvoření jmen a datumu narození, konstruktorů

--- src/Guest.java
+++ src/Guest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Guest {
+    private String name;
+    private LocalDate birthday;
+    private int room;
+    private boolean vacationtype;
+    private int booking;
+
+    public Guest() {
+        this.name = name;
+        this.birthday = birthday;
+        this.room = room;
+        this.vacationtype = vacationtype;
+        this.booking = booking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //System.out.println("Hello world!");//
+
+        Guest guest = new Guest();
+        guest.setName("Adéla Malíková");
+        System.out.println(guest.getName());
+
+        guest.setBirthday("1993, 3, 13");
+        System.out.println(guest.getBirthday());
+
+        guest.setName("Jan Dvořáček");
+        System.out.println(guest.getName());
+
+        guest.setBirthday("1995, 5, 5");
+        System.out.println(guest.getBirthday());
     }
+
 }
